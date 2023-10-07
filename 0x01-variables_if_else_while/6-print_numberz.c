60764aea8a22acec7f4ca2dba32221456f943fe2
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-06 18:40:08 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function - print out numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		putchar('0' + i);
+	}
+
+	putchar('\n');
+
+	return 0;
+}

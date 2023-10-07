76fc5e6cd2e95eb09b9651c2f80ce70dd808a1d1
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-06 18:06:33 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function - print out letters in lowercase, except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n = 'a';
+
+	while (n <= 'z')
+	{
+		if (n != 'e' && n != 'q')
+		{
+			putchar(n);
+		}
+		n++
+	}
+	
+	putchar('\n');
+	return (0);
+}

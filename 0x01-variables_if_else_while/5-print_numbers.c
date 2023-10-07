43c0e61f670c275d84ed7624d61aea12c2e9078e
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-06 18:17:07 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Function - print out numbers of base 10
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		print("%d\n", n);
+	}
+
+	return (0);
+}

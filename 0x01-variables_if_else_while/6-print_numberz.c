06986cdf36a9527a7e8617923cc591d180159330
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-09 13:14:52 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,17 +7,13 @@
  */
 int main(void)
 {
-	putchar('0');
-	putchar('1');
-	putchar('2');
-	putchar('3');
-	putchar('4');
-	putchar('5');
-	putchar('6');
-	putchar('7');
-	putchar('8');
-	putchar('9');
-	putchar('\n');
+	int n;
 
-	return 0;
+	for (n = 0 ; n < 10 ; n++)
+	{
+		putchar(n + '0');
+	}
+
+	putchar('\n');
+	return (0);
 }

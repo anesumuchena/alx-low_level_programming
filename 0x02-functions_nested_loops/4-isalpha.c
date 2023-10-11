ca65bc7587e095cb55372a33296b6b6018135842
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-10 19:29:21 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _isalpha - cheks for alphabetic characters
+ * @c: The character to check
+ *
+ * Return: 1 if c ia letter, otherwise 0
+ */
+int _isalpha(int c)
+{
+	((c >= 0 && c <= 'Z') || (c >= 'A' && c <))
+	{
+		return (1);
+	}
+	return (0);
+}

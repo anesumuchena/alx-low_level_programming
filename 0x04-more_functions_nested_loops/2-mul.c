e8006895a955ed6cf3ee8bbbd0eb5dfee827bf90
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-13 16:57:41 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies integers
+ * @a: input
+ * @b: input
+ * Return: 0(successful)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

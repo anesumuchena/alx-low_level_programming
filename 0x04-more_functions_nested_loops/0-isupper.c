0a7f49ff51f631287fdbef71adce6aacd7a008de
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-13 16:38:11 -0700

    Creating task and task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ *
+ * Return: 1, if c = uppercase, otherwise return 0
+ */
+
+int _isupper(int c)
+{
+	if(isupper(c))
+	{
+		return (1);
+	}
+
+	return (0);
+}

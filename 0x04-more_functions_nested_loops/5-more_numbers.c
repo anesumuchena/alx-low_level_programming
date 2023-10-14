70fcd8b1ff5ab5c394e3866d58cb3d142c969c4d
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-13 19:16:29 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -15,13 +15,9 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j < 10)
-			{
-				_putchar(j + '0');
-			} else
-			{
-				_putchar('0' + (j / 10) * 10 + (j % 10));
-			}
+			if (j >= 10)
+				_putchar('0' + (j / 10));
+			_putchar('0' + (j % 10));
 		}
 		_putchar('\n');
 	}

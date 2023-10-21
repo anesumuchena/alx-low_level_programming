ad15aa694af1be65a1e44bcebb56df31bce58595
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-20 17:05:19 -0700

    tas 0-2

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: number
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != src[n])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

a585f419e12e014d8ed3a66a08160414df58250d
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-10-28 04:09:57 -0700

    task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -31,7 +31,6 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
 				if (result < 10)
 				{
 					_putchar(' ');
@@ -39,6 +38,7 @@ void print_times_table(int n)
 				} else
 				{
 					_putchar((result / 10) + '0');
+					_putchar(' ');
 				}
 				_putchar((result % 10) + '0');
 				}

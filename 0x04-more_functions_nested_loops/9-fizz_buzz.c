4556e4a827cc5140dfc6d649cbdf231f7e3e361a
anesumuchena <117358005+anesumuchena@users.noreply.github.com>
2023-11-04 03:56:12 -0700

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * fizz-buzz - print out:
+ * Fizz at multiples of 3
+ * Buzz at multples of 5
+ * FizzBuzz at multiples of both 3 & 5
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	int n = 1;
+
+	while(n <= 100)
+	{
+		if(n % 3 ==0 && n % 5 == 0)
+		{
+			printf("FizzBuzz");
+		} else if(n % 3 == 0)
+		{
+			printf("Fizz");
+		} else if(n % 5 == 0)
+		{
+			printf("Buzz");
+		} else
+		{
+			printf("%d", n);
+		}
+
+		if(n < 100)
+		{
+			printf(" ");
+		} else
+		{
+			printf("\n");
+		}
+		n++;
+	}
+	return (0);
+}
